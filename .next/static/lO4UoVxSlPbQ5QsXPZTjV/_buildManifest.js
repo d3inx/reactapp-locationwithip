b1ecec991783ff30bdefe31d7cc5d54d3c8e7358
d3inx <mr.reza.rm8224@gmail.com>
2022-04-28 18:25:33 +0430

    main

---
+++ .next/static/lO4UoVxSlPbQ5QsXPZTjV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/687-af9405c14181c8e1.js","static/chunks/pages/index-41a36fee565565ac.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
